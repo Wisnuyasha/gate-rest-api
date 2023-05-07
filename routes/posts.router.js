7ee9fc284a23cf4fed1322d68f8a5a6ccd52fced
RenDaemon <rendieyudh@gmail.com>
2023-05-07 13:41:32 +0700

    fix : log + status

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -5,6 +5,6 @@ const postsController = require("../controller/posts.controller")
 
 router.post("/keluar", postsController.keluar)
 router.post("/masuk", postsController.masuk)
-router.get("/test",postsController.test)
+// router.get("/test",postsController.test)
 
 module.exports = router
